1606e9bcad39edec4860b571b9968a6bf79a6c61
matt-teng <mattteng19980523@gmail.com>
2018-08-23 15:38:51 +1200

    Update ansible.sh

--- ansible.sh
+++ ansible.sh
@@ -43,11 +43,10 @@ wget https://raw.githubusercontent.com/matt-teng/IT-project_Group11/master/wp_gr
 sudo chmod 777 wp_group11.pem
 cp -i wp_group11.pem ~/.ssh/
 
-sudo chmod 600 ~/.ssh/wp_group11.pem
+sudo chmod 400 ~/.ssh/wp_group11.pem
 
 eval $(ssh-agent -s)
 # eval `ssh-agent bash`
 ssh-add ~/.ssh/wp_group11.pem
-sudo chmod 400 ~/.ssh/wp_group11.pem
-# eval `ssh-agent -k`
+
 # export ANSIBLE_HOST_KEY_CHECKING=False
